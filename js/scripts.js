4e78b1cc92a5694abde9c6a635a2d77b3f6d275a
vcarter231 <108429301+vcarter231@users.noreply.github.com>
2022-08-22 11:19:47 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+$(window).scroll(scrollFunction);
+
+
+function scrollFunction() {
+  if ( $(window).scrollTop() > 20 ) {
+    $("#go-top-button").show();
+  } else {
+    $("#go-top-button").hide();
+  }
+};
+
+
+function topFunction() {
+  $(window).scrollTop(0);
+}
